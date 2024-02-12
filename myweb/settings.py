6e8d14bcc8f80757aebe244726f9e3d9ceced0d7
nicelocation <nicelocation1989@gmail.com>
2024-02-12 18:01:44 +0900

    '커밋'

--- myweb/settings.py
+++ myweb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '기본값')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['34.47.69.234']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -134,7 +134,7 @@ LOGGING = {
         'file': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
-            'filename': '/path/to/django/error.log',
+            'filename': 'log/error.log',
         },
     },
     'loggers': {
