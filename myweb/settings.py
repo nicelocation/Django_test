b80b291018daf6010ca2fd4a0b3d192643017aea
nicelocation <nicelocation1989@gmail.com>
2024-02-12 18:09:53 +0900

    '커밋'

--- myweb/settings.py
+++ myweb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '기본값')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['34.47.69.234']
 
 
 # Application definition
